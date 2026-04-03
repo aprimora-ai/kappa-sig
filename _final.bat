93fa4cc31662e9ca1990b0dd5a11d331c1a88056
David <odavidohio@gmail.com>
2026-04-03 14:29:07 -0300

    cleanup

---
+++ _final.bat
@@ -0,0 +1,4 @@
+cd /d C:\Users\ohiod\Projects\kappa-sig
+git add -A
+git commit -m "cleanup"
+git push origin main
--- _push2.bat
+++
@@ -1,7 +0,0 @@
-cd /d C:\Users\ohiod\Projects\kappa-sig
-git add -A
-git reset -- _check.bat _push.bat
-git commit -m "Complete repo: engine, SIG, Sentinel, data, all domains verified"
-git push origin main
-del _check.bat 2>nul
-del _push.bat 2>nul
